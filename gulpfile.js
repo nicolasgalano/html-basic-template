f653068144915fefb62cc5a5d991c25434d6f003
Juan Marchioli <juan@mobomo.com>
2016-10-17 12:52:23 -0300

    added gulp-rev and run-sequence

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
+var rev = require('gulp-rev');
+var runSequence = require('run-sequence');
 
 /** TODO add:
  * gulp-rev
@@ -14,20 +16,27 @@ var plumber = require('gulp-plumber');
 
 var build = require('./build.config.json');
 
-
 gulp.task('templates', function buildHTML() {
+    var manifest = require('./css/rev-manifest.json');
+
     gulp.src(build.app_files.pug)
         .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(pug({
-            pretty: true
+            pretty: true,
+            locals: {
+                css: manifest['app.css']
+            }
         }))
         .pipe(gulp.dest('./'))
 });
 
 gulp.task('sass', function () {
   return gulp.src(build.app_files.sass)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+      .pipe(sass().on('error', sass.logError))
+      .pipe(rev())
+      .pipe(gulp.dest('./css'))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest('./css'));
 });
 
 gulp.task('autoprefixer', function () {
@@ -35,16 +44,43 @@ gulp.task('autoprefixer', function () {
         .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
 });
 
-gulp.task('default', ['templates', 'sass']);
-gulp.task('watch', ['templates', 'sass'], function(){
-    var jadeWatcher = gulp.watch('src/pug/**/*.pug', ['templates']);
-    jadeWatcher.on('change', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-	var sassWatcher = gulp.watch('src/sass/**/*.scss', ['sass']);
-    sassWatcher.on('change', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+//gulp.task('default', ['sass', 'templates']);
+gulp.task('default', function(callback){
+    runSequence('sass',
+                'templates',
+                callback);
+});
+//gulp.task('watch', ['sass', 'templates'], function(){
+//    var jadeWatcher = gulp.watch('src/pug/**/*.pug', ['templates']);
+//    jadeWatcher.on('change', function(event) {
+//        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//    });
+//	var sassWatcher = gulp.watch('src/sass/**/*.scss', function(callback){
+//        runSequence('sass',
+//            'templates',
+//            callback);
+//    });
+//    sassWatcher.on('change', function(event) {
+//        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//    });
+//});
+gulp.task('watch', function(callback){
+    runSequence('sass',
+        'templates',
+        function(){
+            var jadeWatcher = gulp.watch('src/pug/**/*.pug', ['templates']);
+            jadeWatcher.on('change', function(event) {
+                console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+            var sassWatcher = gulp.watch('src/sass/**/*.scss', function(){
+                runSequence('sass',
+                    'templates'
+                    );
+            });
+            sassWatcher.on('change', function(event) {
+                console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+        });
 });
 
 
