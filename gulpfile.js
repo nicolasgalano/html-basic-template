57d5a9cef58f5494386a865212b8f89b7e61e78f
anairamzap <marianapax@gmail.com>
2017-06-27 22:36:02 -0300

    integrate kss into gulp + add required packages + adding docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
+// #############################
+// Init Vars.
+// #############################
+var gulp         = require('gulp'),
+    $            = require('gulp-load-plugins')(),
+    pug          = require('gulp-pug'),
+    sass         = require('gulp-sass'),
+    postcss      = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    notify       = require('gulp-notify'),
+    plumber      = require('gulp-plumber'),
+    path         = require('path'),
+    options      = {};
 
 /** TODO add:
  * gulp-rev
@@ -12,8 +18,52 @@ var plumber = require('gulp-plumber');
  * gulp-livereload
  * */
 
-var build = require('./build.config.json');
+//KSS styleguide
+// #############################
+// Editable paths and options.
+// #############################
+options.rootPath = {
+  project     : __dirname + '/',
+  styleGuide  : __dirname + '/styleguide/',
+ //theme		  : __dirname + '/',
+  src         : __dirname + '/src/'
+};
 
+options.theme = {
+  name       : 'Mobomo Styleguide :)',
+  root       : options.rootPath.project,
+  components : options.rootPath.src     + 'sass/components/',
+  sass       : options.rootPath.src     + 'sass/',
+  css        : options.rootPath.project + 'css/',
+  js         : options.rootPath.project + 'js/'
+};
+
+options.styleGuide = {
+  source: [
+    options.theme.sass
+  ],
+  mask: /\.less|\.sass|\.scss|\.styl|\.stylus/,
+  destination: options.rootPath.styleGuide,
+  // HERE we can add our own builder, or use the bootstrap one? added below as example. 
+  // We should also include it in the node_modules dir.
+  //builder: 'builder/twig',
+
+  // The css and js paths are URLs, like '/misc/jquery.js'.
+  // The following paths are relative to the generated style guide.
+  css: [
+    path.relative(options.rootPath.styleGuide, options.theme.css + 'app.css'),
+    path.relative(options.rootPath.styleGuide, options.theme.css + 'style-guide/chroma-kss-styles.css'),
+    //path.relative(options.rootPath.styleGuide, options.theme.css + 'style-guide/kss-only.css')
+  ],
+  js: [
+  ],
+
+  homepage: 'homepage.md',
+  title: 'Test KSS gulp =^..^='
+};
+
+
+var build = require('./build.config.json');
 
 gulp.task('templates', function buildHTML() {
     gulp.src(build.app_files.pug)
@@ -35,8 +85,31 @@ gulp.task('autoprefixer', function () {
         .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
 });
 
+// #########################
+// Lint Sass.
+// #########################
+gulp.task('lint', ['lint:sass']);
+
+// Lint Sass.
+gulp.task('lint:sass', function () {
+  return gulp.src(options.theme.components + '**/*.scss')
+    .pipe($.sassLint())
+    .pipe($.sassLint.format());
+});
+
+// Lint Sass and throw an error for a CI to catch.
+gulp.task('lint:sass-with-fail', function () {
+  return gulp.src(options.theme.components + '**/*.scss')
+    .pipe($.sassLint())
+    .pipe($.sassLint.format())
+    .pipe($.sassLint.failOnError());
+});
+
+// #########################
+// Gulp Default and Watch
+// #########################
 gulp.task('default', ['templates', 'sass']);
-gulp.task('watch', ['templates', 'sass'], function(){
+gulp.task('watch', ['templates', 'sass','styleguide'], function(){
     var jadeWatcher = gulp.watch('src/pug/**/*.pug', ['templates']);
     jadeWatcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
@@ -45,8 +118,32 @@ gulp.task('watch', ['templates', 'sass'], function(){
     sassWatcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
+
+	var styleguideWatcher = gulp.watch('src/sass/**/*.scss', ['sass']);
+    styleguideWatcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+
+});
+gulp.task('watch:ls', ['templates', 'sass','watch:lint-and-styleguide']);
+gulp.task('watch:lint-and-styleguide', ['styleguide', 'lint:sass'], function () {
+  return gulp.watch([
+    options.theme.components + '**/*.scss',
+    options.theme.components + '**/*.twig'
+  ], options.gulpWatchOptions, ['styleguide', 'lint:sass']);
 });
 
+// kss-node 3.0.0-beta1 and later.
+var kss = require('kss');
 
-
-
+gulp.task('styleguide', function() {
+  return kss(options.styleGuide);
+});
+// #########################
+// Task for styleguide debug
+// #########################
+// Debug the generation of the style guide with the --verbose flag.
+gulp.task('styleguide:debug', ['styleguide'], function () {
+  options.styleGuide.verbose = true;
+  return kss(options.styleGuide);
+});
---
+++ src/sass/homepage.md
@@ -0,0 +1,23 @@
+Here's an example KSS comment:
+<pre class="prettyprint linenums lang-css"><code data-language="css">/*
+Button
+
+Your standard button suitable for clicking.
+
+:hover   - Highlights when hovering.
+.shiny   - Do not press this big, shiny, red button.
+
+Markup: button.html
+
+Style guide: components.button
+*/
+.button {
+  ...
+}
+.button.shiny {
+  ...
+}
+</code></pre>
+
+**For more information on how to write KSS comments, see the [KSS spec](https://github.com/kss-node/kss/blob/spec/SPEC.md).**
+
